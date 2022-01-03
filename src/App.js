c68834ca63e1fc180fe036a2de168077e90dd6e5
Kendall Wong <kwong@Kendalls-MBP.home>
2022-01-02 23:09:57 -0500

    sharing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
-import { withAuthenticator } from '@aws-amplify/ui-react'
 import awsExports from "./aws-exports";
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import AuthClass from '@aws-amplify/ui-react'
+import { ConsoleLogger } from '@aws-amplify/core';
 Amplify.configure(awsExports);
 
-const initialState = { name: '', description: '' }
+const initialState = { name: '', description: ''}
 
 const App = () => {
   const [formState, setFormState] = useState(initialState)
@@ -24,6 +26,8 @@ const App = () => {
   async function fetchTodos() {
     try {
       const todoData = await API.graphql(graphqlOperation(listTodos))
+      console.log("Getting todos")
+      console.log(todoData)
       const todos = todoData.data.listTodos.items
       setTodos(todos)
     } catch (err) { console.log('error fetching todos') }
@@ -35,15 +39,38 @@ const App = () => {
       const todo = { ...formState }
       setTodos([...todos, todo])
       setFormState(initialState)
+      console.log("Getting User ID")
+      var userID = getUserID()
+      console.log("User id" + userID)
       await API.graphql(graphqlOperation(createTodo, {input: todo}))
     } catch (err) {
       console.log('error creating todo:', err)
     }
   }
 
+  function getUserData(){
+    var userData = null
+    for(var key in localStorage) {
+      if (key.includes("userData")) {
+          userData = JSON.parse(localStorage.getItem(key))
+          break
+      }
+    }
+    if (userData == null) {
+      console.log('Problem getting user data')
+    }
+    return userData
+  }
+
+  function getUserID() {
+    var userData = getUserData()
+    var userID = userData['UserAttributes'][0]['Value']
+    return userID
+  } 
+
   return (
     <div style={styles.container}>
-      <h2>Amplify Todos</h2>
+      <h2>Amplify Todos: User #{getUserID()}</h2>
       <input
         onChange={event => setInput('name', event.target.value)}
         style={styles.input}
@@ -64,7 +91,7 @@ const App = () => {
             <p style={styles.todoDescription}>{todo.description}</p>
           </div>
         ))
-      }
+      } 
     </div>
   )
 }
